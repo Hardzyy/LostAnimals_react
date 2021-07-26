854ebf388cb1f7778295c3b03e6f611009b26026
Hardzyy <karbovskiyma@gmail.com>
2021-07-26 13:42:48 +0300

    changed feed

--- src/ToDo/Todoitem.js
+++ src/ToDo/Todoitem.js
@@ -1,12 +1,35 @@
 import React from "react";
 
-export default function Todoitem({ anno, index }){
+const contain = {
+    width: "auto",
+    height: "450px",
+    margin: "1%"
+}
+
+const image = {
+    objectFit: "cover",
+    borderRadius: "5px",
+    width: "400px",
+    height: "400px",
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto"
+}
+
+export default function Todoitem({anno}){
     return (
-        <div>
-            <img
-                src={anno.url}
-                alt="new"
-            />
+        <div style={contain}>
+            <div style={{width: "450px", height:"450px", float:"left"}}>
+                <img style={image}
+                     src={anno.url}
+                     alt="new"
+                />
+            </div>
+                <h4>{anno.title}</h4>
+                <p>Порода: {anno.type} Квартал: {anno.square}</p>
+            <div>
+
+            </div>
         </div>
     );
 }
