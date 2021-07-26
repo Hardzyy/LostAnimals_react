9eb287bde640df5ea0916883af7ec0f790cc7ad4
Hardzyy <karbovskiyma@gmail.com>
2021-07-26 22:21:20 +0300

    again layout

--- src/Announcement/ItemFeed.js
+++ src/Announcement/ItemFeed.js
@@ -1,43 +1,36 @@
 import React from "react";
 import {YMaps, Map, Polygon} from "react-yandex-maps";
-
-const contain = {
-    width: "auto",
-    height: "450px",
-    margin: "1%"
-}
+import {Container, Row, Col} from "reactstrap";
 
 export default function ItemFeed({anno}){
     const mapState = {
-        center: [55.76, 37.64],
-        zoom: 10
+        center: anno.center,
+        zoom: 14
     };
 
     const POLYGON_COORDS = [
-        [[55.75, 37.8], [55.8, 37.9], [55.75, 38.0], [55.7, 38.0], [55.65, 37.9], [55.7, 37.8]]
+        anno.cords
     ];
 
     return (
-        <div style={contain}>
-            <div className="container" style={{width: "450px", height:"450px", float:"left"}}>
-                <img className="image"
-                     src={anno.img_url}
-                     alt="Not loaded"
-                />
-                <div className="overlay">
-                    <div className="text"><a className="lost" href={anno.source_url} target={"_blank"}><strong>Нашли питомца?</strong></a></div>
-                </div>
-                <p style={{textAlign:"center"}}>{anno.date}</p>
-            </div>
-                <div style={{width: "450px", height:"450px", float:"left"}}>
+        <Container style={{margin: "2%"}}>
+            <Row>
+                <Col>
+                    <img className="image"
+                         src={anno.img_url}
+                         alt="Not loaded"
+                    />
+                    <p style={{textAlign:"center"}}>{anno.date}</p>
+                </Col>
+                <Col>
                     <h4>{anno.title}</h4>
                     <p><span>Порода: {anno.type}</span><span> Квартал: {anno.square}</span></p>
                     <div>{anno.description}</div>
-                </div>
-                <div style={{float:"right", margin:"auto"}}>
+                </Col>
+                <Col>
                     <YMaps>
                         <div>Территория вблизи места потери</div>
-                        <Map defaultState={mapState}>
+                        <Map defaultState={mapState} width="80%" heigth="80%">
                             <Polygon
                                 geometry={POLYGON_COORDS}
                                 options={{
@@ -50,9 +43,8 @@ export default function ItemFeed({anno}){
                             />
                         </Map>
                     </YMaps>
-                </div>
-            <div>
-            </div>
-        </div>
+                </Col>
+            </Row>
+        </Container>
     );
 }
