80782266153f595c298de30ae3e6b222ca766bf2
Hardzyy <karbovskiyma@gmail.com>
2021-07-26 11:16:20 +0300

    bootstrap and navbar added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Todolist from "./ToDo/Todolist";
+import Navbarr from "./Navbarr";
 
 function App() {
+
+
     const todos = [
         {id:1, completed: false, title:"Купить хлеб"},
         {id:2, completed: false, title:"Купить milk"},
         {id:3, completed: false, title:"Купить cheese"},
     ]
   return (
-      <div className="wrapper">
-          <h1>React first app</h1>
-          <Todolist todos={todos}/>
+      <div>
+          <Navbarr/>
+          <div className="wrapper">
+              <h1>React first app</h1>
+              <Todolist todos={todos}/>
+          </div>
       </div>
   )
 }
