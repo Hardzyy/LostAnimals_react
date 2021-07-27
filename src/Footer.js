f5b7d5f311bf8aa47e13d3843e325301cc5d0fb9
Hardzyy <karbovskiyma@gmail.com>
2021-07-27 17:32:57 +0300

    creal footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div style={{background:"#CFB6D7",borderTop:"solid", height:"100px"}}>
-            some text
+
         </div>
     )
 }
